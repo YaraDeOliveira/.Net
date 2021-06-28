c2b930b646f1bd6a66af0c26f28b2c43a5b28347
Yara Regina <yara.regina@hotmail.com>
2021-06-28 16:30:21 -0300

    Lambda, linq e delegates

---
+++ TreinandoLinq/TreinandoLinq/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace TreinandoLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Especificar o data source
+
+            int[] numers = new int[] { 2, 3, 4, 5 };
+
+
+            // Definir a consulta - query expression
+            // Pegar todos os numeros pares e multiplica-los por 10
+            var result = numers.Where(x => x % 2 == 0).Select(x => x *10);
+
+
+            //Execute de query 
+            foreach (int x in result)
+            {
+                Console.WriteLine(x);
+            }
+
+
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ UsandoFunc/UsandoFunc/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using UsandoFunc.Entities;
+using System.Linq;
+
+namespace UsandoFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+            // um metodo
+            //Func<Product, string> func = NameUpper;
+            //List<string> result2 = list.Select(func).ToList();
+            //foreach (string s in result2)
+            //{
+            //    Console.WriteLine(s);
+            //}
+
+            //Lambda inline
+            //List<string> result = list.Select(p => p.Name.ToUpper()).ToList();
+
+
+
+
+
+            // Outro modo de fazer
+            List<string> result = list.Select(NameUpper).ToList();
+            foreach(string s in result)
+            {
+                Console.WriteLine(s);
+            }
+
+        }
+        static string NameUpper(Product p)
+        {
+            return p.Name.ToUpper();
+        }
+    }
+}
